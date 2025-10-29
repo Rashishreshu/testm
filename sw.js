df96eb17754383ff57275d1b0773b6915bbcd584
Rashishreshu <155653198+Rashishreshu@users.noreply.github.com>
2025-10-29 17:00:03 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,49 +1,43 @@
 const CACHE_NAME = 'todo-pwa-cache-v1';
 const urlsToCache = [
-  '/', // Caches the root path
-  '/index.html',
-  '/manifest.json',
-  // Icon URLs (since they are external, caching them is good for offline use)
-  'http://googleusercontent.com/image_collection/image_retrieval/2048319554149166403_0',
-  'http://googleusercontent.com/image_collection/image_retrieval/2048319554149166403_1'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png'
 ];
 
-// Install Event: caches the required assets
+// Install event: cache essential files
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Service Worker: Caching App Shell');
+        console.log('Files cached successfully');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-// Fetch Event: serves assets from the cache first (Cache-First Strategy)
+// Fetch event: serve cached content when offline
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return the cached response
-        if (response) {
-          return response;
-        }
-        // No cache match - fetch from the network
-        return fetch(event.request);
+        // If cache found, return it; else fetch from network
+        return response || fetch(event.request);
       })
   );
 });
 
-// Activate Event: cleans up old caches
+// Activate event: clear old caches if version changes
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            console.log('Service Worker: Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            console.log('Old cache cleared:', name);
+            return caches.delete(name);
           }
         })
       );
